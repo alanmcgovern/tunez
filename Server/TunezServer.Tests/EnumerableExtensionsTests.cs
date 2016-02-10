a63c7ff2a47fd6de0d851d5f6dccda2f0ed05a43
Alan McGovern <alan@xamarin.com>
2016-02-10 01:51:59 +0000

    [tests] Add some helpers for checking permutations
    I'm going to use this to ensure that we always properly
    sort the items going into our Catalog.

--- Server/TunezServer.Tests/EnumerableExtensionsTests.cs
+++ Server/TunezServer.Tests/EnumerableExtensionsTests.cs
@@ -30,6 +30,32 @@ namespace TunezServer
 			foreach (var p in partition)
 				Assert.AreEqual (25, p.Count (), "#2");
 		}
+
+		[Test]
+		public void Permutations_ThreeItems ()
+		{
+			var permutations = Enumerable.Range (1, 3).AllPermutations ().ToArray ();
+			Assert.AreEqual (6, permutations.Length, "#1");
+			for (int i = 0; i < permutations.Length; i ++) {
+				for (int j = 0; j < permutations.Length; j ++) {
+					if (i != j)
+						Assert.IsFalse (permutations [i].SequenceEqual (permutations [j]));
+				}
+			}
+		}
+
+		[Test]
+		public void Permutations_FourItems ()
+		{
+			var permutations = Enumerable.Range (1, 4).AllPermutations ().ToArray ();
+			Assert.AreEqual (24, permutations.Length, "#1");
+			for (int i = 0; i < permutations.Length; i ++) {
+				for (int j = 0; j < permutations.Length; j ++) {
+					if (i != j)
+						Assert.IsFalse (permutations [i].SequenceEqual (permutations [j]));
+				}
+			}
+		}
 	}
 }
 
---
+++ Server/TunezServer.Tests/TestHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TunezServer
+{
+	public static class TestHelpers
+	{
+		public static IEnumerable<IEnumerable<T>> AllPermutations<T> (this IEnumerable<T> enumerable)
+		{
+			var input = enumerable.ToArray ();
+			foreach (var v in AllPermutations (input, 0, input.Length))
+				yield return v;
+		}
+
+		static IEnumerable<T[]> AllPermutations<T> (T[] array, int start, int end)
+		{
+			if (start == end - 1) {
+				yield return array.ToArray (); // clone it
+			} else {
+				foreach (var v in AllPermutations (array, start + 1, end))
+					yield return v;
+
+				for (int i = start + 1; i < end; i ++) {
+					// swap two to create a permutation
+					var tmp = array [i];
+					array [i] = array [start];
+					array [start] = tmp;
+
+					foreach (var v in AllPermutations (array, start + 1, end))
+						yield return v;
+
+					// Swap them back so we can try the next permutation
+					array [start] = array [i];
+					array [i] = tmp;
+				}
+			}
+		}
+	}
+}
+
